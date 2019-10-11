70c06beebc03cca017504342ba29ea2617ed7e09
Gregory Rubinstein <Gregory.E.Rubinstein@gmail.com>
2019-10-10 23:00:09 -0400

    Create Contact.py

---
+++ Contact.py
@@ -0,0 +1,19 @@
+'''
+Contact object should have the following data points:
+  First Name
+  Last Name
+  Middle Initial
+  Home Phone Number
+  Mobile Phone Number
+  Work Phone Number
+  Street Address
+  City
+  State
+  Zip Code
+  Country
+  Email Address
+  Notes
+  Pictures
+ All data points except First Name and Last Name are optional
+'''
+class Contact():
