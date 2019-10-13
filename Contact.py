da85f8fc4a37386f6319cd687783de102b49fb9b
Gregory Rubinstein <gregory.e.rubinstein@gmail.com>
2019-10-13 14:49:31 -0400

    added First_Name and Last_Name

--- Contact.py
+++ Contact.py
@@ -15,9 +15,42 @@ Contact object should have the following data points:
   Notes
   Pictures
  All data points except First Name and Last Name are optional
+
+ class Apartment(House): #Apartment inherits from House
+    def __init__(self, pID, aptNo, rent, isVacant = False):
+        self._pID = pID
+        self._aptNo = aptNo
+        self._rent = rent
+        self._isVacant = isVacant
+
+    @property
+    def pID(self):
+        return self._pID
+
+    @setter.pID
+    def pID(self, pID):
+        self._pID = pID
+
 """
 
 
 class Contact:
-    def __init__(self):
-        pass
+    def __init__(self, fname, lname):
+        self._First_Name = fname
+        self._Last_Name = lname
+
+    @property
+    def First_Name(self, fname):
+        return fname
+
+    @First_Name.setter
+    def First_Name(self, fname):
+        self._First_Name = fname
+
+    @property
+    def Last_Name(self, lname):
+        return lname
+
+    @Last_Name.setter
+    def Last_Name(self, lname):
+        self._Last_Name = lname
